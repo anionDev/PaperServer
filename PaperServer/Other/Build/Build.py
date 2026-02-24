bb06b5b4fb40df722082498fb45b6acc725a53c5
Marius Göcke <marius.goecke@gmail.com>
2026-02-25 00:59:21 +0100

    udpate due to adapted build-script-framework

--- PaperServer/Other/Build/Build.py
+++ PaperServer/Other/Build/Build.py
@@ -1,11 +1,12 @@
 from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_CodeUnitSpecific_Docker_Functions,TFCPS_CodeUnitSpecific_Docker_CLI
 
-
+ 
 def build():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.build(None,{
-        "debian":tf._protected_sc.default_fallback_docker_registry,
+    tf.build({
+        "image_debian":tf.tfcps_Tools_General.oci_image_manager.get_registry_address_for_image_with_default_tag(tf.get_repository_folder(),"Debian"),
     })
+    #TODO add sbom from upstream
 
 if __name__ == "__main__":
     build()
