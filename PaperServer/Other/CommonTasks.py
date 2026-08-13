1ccfb069f20953d9a5303c88ffd4458fd1e3c501
Marius Göcke <marius.goecke@gmail.com>
2026-08-13 13:00:54 +0200

    update in script

--- PaperServer/Other/CommonTasks.py
+++ PaperServer/Other/CommonTasks.py
@@ -25,6 +25,7 @@ def common_tasks():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
     download_paperserver()
     tf.do_common_tasks(tf.get_version_of_project())#codeunit-version should alsways be the same as project-version
+    tf.take_readmefile_from_main_readmefile_of_repository(file)
 
 
 if __name__ == "__main__":
